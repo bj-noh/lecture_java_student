5b63404121b4f3d16c21e4ec5dae6d25dd38fcdc
Byeongjoon <powernoh92@gmail.com>
2023-03-20 16:26:56 +0900

    chapter 3 uploaded

---
+++ Chap3_Example/_08_NestedLoop.java
@@ -0,0 +1,28 @@
+package Chap3_Example;
+
+public class _08_NestedLoop {
+    public static void main(String[] args) {
+        // 이중 반복문
+
+        // 별 (*) 사각형 만들기
+        /*
+
+        *****
+        *****
+        *****
+        *****
+        *****
+
+         */
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println("-----------------");
+
+        
+    }
+}
---
+++ Chap3_Example/_12_PrimeNumberSimple.java
@@ -0,0 +1,32 @@
+package Chap3_Example;
+
+import java.util.Scanner;
+
+public class _12_PrimeNumberSimple {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+
+        boolean isPrime = true;
+
+        if (num <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i < num; i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime) {
+            System.out.println(num + " is a prime number.");
+            
+        } else {
+            System.out.println(num + " is not a prime number.");
+        }
+    }
+}
