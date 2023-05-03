681a69fa4881da4d2f8532e1e647a29f384fef02
Byeongjoon <powernoh92@gmail.com>
2023-05-03 11:04:09 +0900

    StudentTest updated

--- 2023_java/src/Chap6_Practice_InClass/StudentTest.java
+++ 2023_java/src/Chap6_Practice_InClass/StudentTest.java
@@ -5,12 +5,15 @@ public class StudentTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Student[] students = new Student[100];
+		Student[] students = new Student[5];
 		
 		for (int i = 0; i < students.length; i++) {
 			students[i] = new Student(i+1);
-			System.out.println("Camera num: " + cameraArr[i].camId);
-		}	
+		}
+		
+		for (int i = 0; i < students.length; i++) {
+			students[i].showStudentInfo();
+		}		
 
 	}
 
