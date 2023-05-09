1d62bbf9772e101abe13192f81956a353b125d91
Byeongjoon <powernoh92@gmail.com>
2023-05-10 08:12:47 +0900

    assignment07_03 updated

--- 2023_java/assignment/assignment07_03/EmployeeTest.java
+++ 2023_java/assignment/assignment07_03/EmployeeTest.java
@@ -1,5 +1,8 @@
 package assignment07_03;
 
+import assignment07_03_answer.HourlyEmployee;
+import assignment07_03_answer.SalariedEmployee;
+
 public class EmployeeTest {
     public static void main(String[] args) {
     	/*
@@ -9,11 +12,41 @@ public class EmployeeTest {
     	 * 결과>
     	 * 
     	 *	Hourly Employee: John Doe, Employee ID: 1001, Department: IT, Hourly Rate: $15.0, Hours Worked: 40.0, Weekly Salary: $600.0
-			Salaried Employee: Jane Smith, Employee ID: 1002, Department: HR, Monthly Salary: $60000.0, Weekly Salary: $1153.8461538461538
+			Salaried Employee: Jane Smith, Employee ID: 1002, Department: HR, Monthly Salary: $5000.0, Weekly Salary: $60000.0
 			=== After change John's hourly rate and hours worked into 18 and 50, respectively ===
 			=== After change Jane's yearly salary into $ 65000 ===
 			Hourly Employee after raise: John Doe, Employee ID: 1001, Department: IT, Hourly Rate: $18.0, Hours Worked: 50.0, Weekly Salary: $900.0
-			Salaried Employee after raise: Jane Smith, Employee ID: 1002, Department: HR, Yearly Salary: $65000.0, Weekly Salary: $1250.0
+			Salaried Employee after raise: Jane Smith, Employee ID: 1002, Department: HR, Monthly Salary: $5416.666666666667, Yearly Salary: $65000.0
 
-    	 */    	
+    	 */
+    	
+        HourlyEmployee hourlyEmployee = new HourlyEmployee("John", "Doe", 1001, 15.0, 40.0, "IT");
+        SalariedEmployee salariedEmployee = new SalariedEmployee("Jane", "Smith", 1002, 60000.0, "HR");
+        
+        System.out.println("Hourly Employee: " + hourlyEmployee.getFullName() + ", Employee ID: " +
+                hourlyEmployee.getEmployeeId() + ", Department: " + hourlyEmployee.getDepartment() +
+                ", Hourly Rate: $" + hourlyEmployee.getHourlyRate() + ", Hours Worked: " +
+                hourlyEmployee.getHoursWorked() + ", Weekly Salary: $" + hourlyEmployee.getSalary());
+
+        System.out.println("Salaried Employee after raise: " + salariedEmployee.getFullName() + ", Employee ID: " +
+                salariedEmployee.getEmployeeId() + ", Department: " + salariedEmployee.getDepartment() +
+                ", Monthly Salary: $" + salariedEmployee.getMonthlySalary() + ", Yearly Salary: $" +
+                salariedEmployee.getSalary());
+        
+        System.out.println("=== After change John's hourly rate and hours worked into 18 and 50, respectively ===");
+        hourlyEmployee.setHourlyRate(18.0);
+        hourlyEmployee.setHoursWorked(50.0);
+        
+        System.out.println("=== After change Jane's yearly salary into $ 65000 ===");
+        salariedEmployee.setMonthlySalary(65000.0);
+        
+        System.out.println("Hourly Employee after raise: " + hourlyEmployee.getFullName() + ", Employee ID: " +
+                hourlyEmployee.getEmployeeId() + ", Department: " + hourlyEmployee.getDepartment() +
+                ", Hourly Rate: $" + hourlyEmployee.getHourlyRate() + ", Hours Worked: " +
+                hourlyEmployee.getHoursWorked() + ", Weekly Salary: $" + hourlyEmployee.getSalary());
+        
+        System.out.println("Salaried Employee after raise: " + salariedEmployee.getFullName() + ", Employee ID: " +
+                salariedEmployee.getEmployeeId() + ", Department: " + salariedEmployee.getDepartment() +
+                ", Monthly Salary: $" + salariedEmployee.getMonthlySalary() + ", Yearly Salary: $" +
+                salariedEmployee.getSalary());
 }
--- 2023_java/assignment/assignment07_03/SalariedEmployee.java
+++ 2023_java/assignment/assignment07_03/SalariedEmployee.java
@@ -9,13 +9,14 @@ public class SalariedEmployee extends Employee {
 	 * 		SalariedEmployee의 월급
 	 *  
 	 * + SalariedEmployee(String firstName, String lastName, int employeeId, double yearlySalary, String department)
-	 * 		입력받은 각 파라미터를 상위 클래스와 현재클래스의 각 변수에 할당하는 생성자 
+	 * 		입력받은 각 파라미터를 상위 클래스와 현재 클래스의 각 변수에 할당하는 생성자 
+	 *      참고) 연봉을 파라미터로 입력받고, 월급으로 저장함 (월급 = 연봉 나누기 12개월)
 	 *  
 	 * + getSalary(): double
-	 * 		SalariedEmployee의 월 급여액을 반환하는 메소드 (연봉 나누기 12개월)
+	 * 		SalariedEmployee의 연봉을 반환하는 메소드 (월급 곱하기 12개월) 
 	 * 
 	 * + setMonthlySalary(double yearlySalary): void
-	 * 		SalariedEmployee의 월급을 설정(변경)하는 메소드
+	 * 		SalariedEmployee의 연봉을 입력받아 월급을 설정(변경)하는 메소드 (연봉 나누기 12개월)
 	 * 
 	 * + getMonthlySalary(): void
 	 * 		SalariedEmployee의 월급을 반환하는 메소드
