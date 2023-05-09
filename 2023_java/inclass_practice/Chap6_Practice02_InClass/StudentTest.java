c4a8627a2ab61b788d22eb57963bdffd03376fc7
Byeongjoon <powernoh92@gmail.com>
2023-05-09 16:12:11 +0900

    chap6 inclass02 .java file updated

--- 2023_java/inclass_practice/Chap6_Practice02_InClass/StudentTest.java
+++ 2023_java/inclass_practice/Chap6_Practice02_InClass/StudentTest.java
@@ -4,20 +4,17 @@ public class StudentTest {
     public static void main(String[] args) {
     	/*
     	 *  결과: 
-    	 *  Freshman Information:
-			Name: John Doe
+    	 *  Name: John Doe
 			Age: 18
 			Major: Computer Science
 			Average Grade: 81.66666666666667
 			
-			Freshman Information:
 			Name: Jane Smith
 			Age: 19
 			Major: Mathematics
 			Average Grade: 88.33333333333333
 			Student ID: 1234
 			
-			Freshman Information:
 			Name: Alex Johnson
 			Age: 20
 			Major: Physics
@@ -25,7 +22,6 @@ public class StudentTest {
 			Student ID: 5678
 			Club: Chess Club
 			
-			Freshman Information:
 			Name: Emily Davis
 			Age: 21
 			Major: Biology
@@ -33,7 +29,10 @@ public class StudentTest {
 			Student ID: 9012
 			Club: Debate Club
 			Graduation Year: 2023
+
+
     	 */
+    	
         // Creating objects of each class
         Freshman freshman = new Freshman("John Doe", 18, "Computer Science", new int[]{80, 75, 90});
         Sophomore sophomore = new Sophomore("Jane Smith", 19, "Mathematics", new int[]{85, 92, 88}, 1234);
@@ -44,17 +43,7 @@ public class StudentTest {
         Student[] students = {freshman, sophomore, junior, senior};
 
         // Displaying student information and average grades
-        for (Student student : students) {
-            if (student instanceof Freshman) {
-                System.out.println("Freshman Information:");
-            } else if (student instanceof Sophomore) {
-            	System.out.println("Sophomore Information:");
-            } else if (student instanceof Junior) {
-            	System.out.println("Junior Information:");
-            } else if (student instanceof Senior) {
-            	System.out.println("Senior Information:");
-            }
-            
+        for (Student student : students) {            
             student.displayInfo();
             System.out.println();
         }
