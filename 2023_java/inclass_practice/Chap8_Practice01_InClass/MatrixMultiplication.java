934dd7e2e3fcf275d66e8c4d5070b8f71a3c1fab
Byeongjoon <powernoh92@gmail.com>
2023-05-22 12:58:35 +0900

    Chap8 Practice

---
+++ 2023_java/inclass_practice/Chap8_Practice01_InClass/MatrixMultiplication.java
@@ -0,0 +1,19 @@
+package Chap8_Practice01_InClass;
+
+public class MatrixMultiplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		// read two matrix from two text files
+		// file path: ./data/Chap8_Example_InClass
+		
+		
+		// calculate
+		
+		// write result as text file
+
+	}
+
+}
